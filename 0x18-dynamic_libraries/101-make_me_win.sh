bf3961a9b6580421f1164afedea43c2a8e954f7f
Maxidonx <johnmaxwell927@gmail.com>
2023-01-16 15:38:49 -0800

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -P /tem https://github.com/Maxidonx/alx-low_level_programing/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/Maxidonx/alx-low_level_programing/raw/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
